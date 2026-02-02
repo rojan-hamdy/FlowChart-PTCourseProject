de5805c8721644c163c3974331671f26c01c4e7d
Rojan Hamdy <rojanhs2@gmail.com>
2026-02-02 19:29:08 +0200

    .

--- Actions/Switch_To_Sim.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Action.h"
-class Switch_To_Sim : public Action
-{
-public:
-	Switch_To_Sim(ApplicationManager* pAppManager);
-	virtual void ReadActionParameters();
-	virtual void Execute();
-};
